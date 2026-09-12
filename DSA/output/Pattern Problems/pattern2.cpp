4648b3225f30d1af1e747521fb2b6ac20d6517b4
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-09-12 17:54:46 +0530

    pattern2

--- DSA/output/Pattern Problems/pattern2.cpp
+++ DSA/output/Pattern Problems/pattern2.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n;
     cout<<"Enter the no : ";
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
             cout<<"*";
-
         }
         cout<<endl;
     }
