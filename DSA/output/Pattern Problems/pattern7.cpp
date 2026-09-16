23184eb2b4491b4d07f31dff61b13ef4c26c6d64
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-09-16 21:30:19 +0530

    pattern7

--- DSA/output/Pattern Problems/pattern7.cpp
+++ DSA/output/Pattern Problems/pattern7.cpp
@@ -1,18 +1,19 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
 int main(){
     int n;
-    cout<<"Enter the no : ";
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
-            cout << "  ";
+    cout<<"Enter the number : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        // for spaces 
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
         }
-        for (int j = 1; j <= 2 * i - 1; j++) {
-            cout << "* ";
+        // for stars 
+        for(int j=1;j<=(2*i-1);j++){
+            cout<<"*";
         }
-        cout << endl;
+        cout<<endl;
     }
     return 0;
 }
