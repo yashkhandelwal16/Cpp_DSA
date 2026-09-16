e31114ee4238e7a625acf5edec1c8c20c05ba3c8
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-09-16 21:52:22 +0530

    pattern9

---
+++ DSA/output/Pattern Problems/pattern9.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    // upperhalf 
+    for(int i=1;i<=n;i++){
+        // for spaces 
+        for(int j=1;j<=(n-i);j++){
+            cout<<" ";
+        }
+        // for stars 
+        for(int j=1;j<=(2*i-1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    // lowerhalf  
+    for(int i=n;i>=1;i--){
+        // for spaces 
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        // for stars 
+        for(int j=1;j<=(2*i-1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
