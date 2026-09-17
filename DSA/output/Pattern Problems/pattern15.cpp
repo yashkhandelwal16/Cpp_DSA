67730b69080f7a03d38d3e581d03dd8f7e8e94ee
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-09-17 22:03:59 +0530

    pattern15

---
+++ DSA/output/Pattern Problems/pattern15.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no : ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        char val = (char)('A'+i);
+        for(int j=0;j<=i;j++){
+            cout<<val;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
