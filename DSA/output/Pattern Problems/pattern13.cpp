51df3c4c1aa33e1c3d47f655940985867b09e238
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-09-17 21:49:15 +0530

    pattern13

---
+++ DSA/output/Pattern Problems/pattern13.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<(char)('A'+j);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
