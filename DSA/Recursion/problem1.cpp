9185485593c5bfe4df249b3a21a02a1e4a5744f3
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-09-19 11:34:31 +0530

    ready to push

--- DSA/Recursion/problem1.cpp
+++ DSA/Recursion/problem1.cpp
@@ -1,10 +1,11 @@
 // Print name N times using recursion 
-#include<bits\stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 void func(string name){
     cout<<name<<endl;
     func(name);
 }
+
 int main(){
     string name;
     cout<<"Enter your name : ";
